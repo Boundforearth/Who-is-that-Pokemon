5532fe91c9ffe028de2309185d8310984208442a
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-02-27 11:55:21 -0500

    Update scripts.js
    Added an array of objects containing Pokemon

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+let pokemonList = [
+  {
+    name: "Clefairy",
+    types: ["Fairy"],
+    "base stats": {
+      hp: 70,
+      attack: 45,
+      defense: 48,
+      speed: 35,
+      "sp atk": 60,
+      "sp def": 65
+    },
+    height: 0.6,
+    weight: 7.5
+  },
+  {
+    name: "Ghastly",
+    types: ["Ghost", "Poison"],
+    "base stats": {
+      hp: 30,
+      attack: 35,
+      defense: 30,
+      speed: 80,
+      "sp atk": 100,
+      "sp def": 35
+    },
+    height: 1.3,
+    weight: 0.1
+  },
+  {
+    name: "Porygon",
+    types: ["Normal"],
+    "base stats": {
+      hp: 65,
+      attack: 60,
+      defesne: 70,
+      speed: 40,
+      "sp atk": 85,
+      "sp def": 75
+    },
+    height: 0.8,
+    weight: 36.5
+  }
+];
