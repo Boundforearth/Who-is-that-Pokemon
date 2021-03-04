0f5e150bc5c967309de4368fd412bc7c7122d61c
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-03-04 11:59:47 -0500

    changed for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
-//Array of Pokemon objects
-let pokemonList = [
+
+  let pokemonList = [
   {
     name: "Clefairy",
     types: ["Fairy"],
@@ -45,51 +45,52 @@ let pokemonList = [
   }
 ];
 
-//for loop to run through the array of Pokemon Objects and print the data contained to the website's page
-for(i = 0; i < pokemonList.length; i++) {
-  //Simple variables for all properties
-  let name = pokemonList[i].name;
-  let type1 = pokemonList[i].types[0];
-  let type2 = pokemonList[i].types[1];
-  let height = pokemonList[i].height;
-  let weight = pokemonList[i].weight
-  let hp = pokemonList[i].baseStats.hp
-  let attack = pokemonList[i].baseStats.attack
-  let defense = pokemonList[i].baseStats.defense
-  let speed = pokemonList[i].baseStats.speed
-  let spAtk = pokemonList[i].baseStats.spAtk
-  let spDef = pokemonList[i].baseStats.spDef
- 
-  document.write(`<h3 class="${type1}">${name}: <br></h3>`);
+  //function used to display all aspects of a Pokemon.  To but entered into a forEach loop.
+  function displayPokemon(pokemon) {
+    let name = pokemon.name;
+    let type1 = pokemon.types[0];
+    let type2 = pokemon.types[1];
+    let height = pokemon.height;
+    let weight = pokemon.weight
+    let hp = pokemon.baseStats.hp
+    let attack = pokemon.baseStats.attack
+    let defense = pokemon.baseStats.defense
+    let speed = pokemon.baseStats.speed
+    let spAtk = pokemon.baseStats.spAtk
+    let spDef = pokemon.baseStats.spDef
+  
+    document.write(`<h3 class="${type1}">${name}: <br></h3>`);
 
-  //Determine whether one or two types, then print text accordingly
-  if (pokemonList[i].types.length === 2) {
-    document.write(`Types - ${type1}, ${type2} <br>`)
-  }
-  else {
-    document.write(`Type - ${type1} <br>`)
+    //Determine whether one or two types, then print text accordingly
+    if (pokemon.types.length === 2) {
+      document.write(`Types - ${type1}, ${type2} <br>`)
+    }
+    else {
+      document.write(`Type - ${type1} <br>`)
+    }
+
+    //Add a special comment about the Pokemon's height.  1.2+ is large, .7- is small
+    //<p> tag added to create more white space between the height and the comment
+    if (height > 1.2){
+      document.write(`Height - ${height}  <p class="size-comment">Wow, that's big!</p> <br>`)
+    }
+    else if (height > .7) {
+      document.write(`Height - ${height}  <p class="size-comment">I'd say that's about average size.</p> <br>`)
+    }
+    else {
+      document.write(`Height - ${height}  <p class="size-comment">That's a really small Pokemon!</p> <br>`)
+    }
+
+    //prints weight and base stats. div added to provide spacing on the left margin
+    document.write(` Weight - ${weight} <br>
+      Base Stats: <br>
+      <div class="base-stats">HP - ${hp} <br>
+      Attack - ${attack} <br>
+      Defense - ${defense} <br>
+      Speed - ${speed} <br>
+      Special Attack - ${spAtk} <br>
+      Special Defense - ${spDef} <br><br>
+      </div>` );
   }
 
-  //Add a special comment about the Pokemon's height.  1.2+ is large, .7- is small
-  //<p> tag added to create more white space between the height and the comment
-  if (height > 1.2){
-    document.write(`Height - ${height}  <p class="size-comment">Wow, that's big!</p> <br>`)
-  }
-  else if (height > .7) {
-    document.write(`Height - ${height}  <p class="size-comment">I'd say that's about average size.</p> <br>`)
-  }
-  else {
-    document.write(`Height - ${height}  <p class="size-comment">That's a really small Pokemon!</p> <br>`)
-  }
-
-  //prints weight and base stats. div added to provide spacing on the left margin
-  document.write(` Weight - ${weight} <br>
-    Base Stats: <br>
-    <div class="base-stats">HP - ${hp} <br>
-    Attack - ${attack} <br>
-    Defense - ${defense} <br>
-    Speed - ${speed} <br>
-    Special Attack - ${spAtk} <br>
-    Special Defense - ${spDef} <br><br>
-    </div>` );
-}
+  pokemonList.forEach(displayPokemon);
