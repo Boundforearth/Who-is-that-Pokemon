40da7d6caa442949a7e2a3f5215e53b57b1ac1e5
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-03-04 13:10:01 -0500

    Added validation
    - used typeof to make sure added Pokemon is an object
    
    -used Object.keys to make sure all initial keys are correct
    
    -used Object.keys to check all keys on baseStats are correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 
 let pokemonRepository = (function() {
+  //static list of Pokemon objects 
   let pokemonList = [
   {
     name: "Clefairy",
     types: ["Fairy"],
+    height: 0.6,
+    weight: 7.5,
     baseStats: {
       hp: 70,
       attack: 45,
@@ -11,13 +14,13 @@ let pokemonRepository = (function() {
       speed: 35,
       spAtk: 60,
       spDef: 65
-    },
-    height: 0.6,
-    weight: 7.5
+    }
   },
   {
     name: "Ghastly",
     types: ["Ghost", "Poison"],
+    height: 1.3,
+    weight: 0.1,
     baseStats: {
       hp: 30,
       attack: 35,
@@ -25,13 +28,13 @@ let pokemonRepository = (function() {
       speed: 80,
       spAtk: 100,
       spDef: 35
-    },
-    height: 1.3,
-    weight: 0.1
+    }
   },
   {
     name: "Porygon",
     types: ["Normal"],
+    height: 0.8,
+    weight: 36.5,
     baseStats: {
       hp: 65,
       attack: 60,
@@ -39,20 +42,37 @@ let pokemonRepository = (function() {
       speed: 40,
       spAtk: 85,
       spDef: 75
-    },
-    height: 0.8,
-    weight: 36.5
+    }
   }
 ];
 
+// function to give access to the pokemonList outside of this IIFE
   function getAll() {
     return pokemonList;
   }
 
+  //function to add additional Pokemon to the pokemon list
   function add(pokemon) {
-    pokemonList.push(pokemon);
+
+    //Checks if input is an object
+    if(typeof(pokemon) !== "object") {
+      return alert("Please write a Pokemon as an object");
+    }
+
+    //sorts and turns array content into strings to check validity of keys.
+    else if (Object.keys(pokemon).sort().join(" ") !== Object.keys(pokemonList[0]).sort().join(" ")) {
+      return alert("Please enter a valid Pokemon (name, types, height, weight, baseStats)");
+    }
+
+    // check that proper keys have been used for all the base stats
+    else if (Object.keys(pokemon.baseStats).sort().join(" ") !== Object.keys(pokemonList[0].baseStats).sort().join(" "))  {
+      return alert("Please enter valid base stats (hp, attack, defense, speed, spAtk, spDef)")
+    }
+
+    else {pokemonList.push(pokemon)};
   }
 
+  //return the functions to give access to them
   return {
     getAll: getAll,
     add: add,
@@ -108,5 +128,5 @@ function displayPokemon(pokemon) {
     </div>` );
 }
 
-
+pokemonRepository.getAll().forEach(displayPokemon);
 
