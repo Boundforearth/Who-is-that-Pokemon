62587baa11b2602121c56a70de91c9f9d44a645d
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-03-08 22:23:31 -0500

    Added and removed functions
    - if statements added to addListItem to group Pokemon generations by class
    
    - chooseList function added to select Pokemon by Generation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 
 let pokemonRepository = (function() {
-  //static list of Pokemon objects 
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=493';
   function loadList() {
-    fetch(apiUrl).then(function (resolve) {
-      return resolve.json();
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
     }).then(function (json) {
       json.results.forEach( function(item) {
         let pokemon = {
@@ -13,25 +12,48 @@ let pokemonRepository = (function() {
           detailsUrl: item.url
         }
         add(pokemon);
-      })}).catch(function(e) {
+      })
+    }).catch(function(e) {
         console.error(e);
       } )
   }
+  function chooseList () {
+    let array = ["1", "2", "3", "4"];
+    for(let i = 0; i < 4; i++) {
+      let genEvent = document.getElementById("gen" + array[i]);
+      genEvent.addEventListener('click', function(){
+        let hide = document.querySelectorAll(".button");
+        hide.forEach(function(element) {
+          element.classList.add("hidden");
+        })
+        let unhide = document.querySelectorAll(".button" + array[i]);
+        unhide.forEach(function(element) {
+          element.classList.remove("hidden");
+        })
+      })
+    }
+  }
+
 
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).this(function (response) {
+    return fetch(url).then(function(response) {
       return response.json();
     }).then(function (details) {
       item.height = details.height;
       item.imageUrl = details.sprites.front_default;
-      image.hp = details.stats[0].base_stat;
-      image.attack = details.stats[1].base_stat;
-      image.defense = details.stats[2].base_stat;
-      image.spAtk = details.stats[3].base_stat;
-      image.spDef = details.stats[4].base_stat;
-      image.speed = details.stats[5].base_stat;
-      item.types = details.types;
+      item.hp = details.stats[0].base_stat;
+      item.attack = details.stats[1].base_stat;
+      item.defense = details.stats[2].base_stat;
+      item.spAtk = details.stats[3].base_stat;
+      item.spDef = details.stats[4].base_stat;
+      item.speed = details.stats[5].base_stat;
+      item.type1 = details.types[0].type.name;
+
+      if (details.types[1]) {
+      item.type2 = details.types[1].type.name;
+      }
+
       item.weight = details.weight;
     }).catch(function (e) {
       console.log(e);
@@ -41,75 +63,75 @@ let pokemonRepository = (function() {
   function chooseButtonClass (type) {
     let newClass;
     switch (type) {
-      case "Bug":
+      case "bug":
         newClass = "bugButton";
         break;
       
-      case "Dark":
+      case "dark":
         newClass = "darkButton";
         break;
 
-      case "Dragon":
+      case "dragon":
         newClass = "dragonButton";
         break;
 
-      case "Electric":
+      case "electric":
         newClass = "electricButton";
         break;
         
-      case "Fairy":
+      case "fairy":
         newClass = "fairyButton";
         break;
   
-      case "Fighting":
+      case "fighting":
         newClass = "fightingButton";
         break;
 
-      case "Fire":
+      case "fire":
         newClass = "fireButton";
         break;
           
-      case "Flying":
+      case "flying":
         newClass = "flyingButton";
         break;
     
-      case "Ghost":
+      case "ghost":
         newClass = "ghostButton";
         break;
 
-      case "Grass":
+      case "grass":
         newClass = "grassButton";
         break;
       
-      case "Ground":
+      case "ground":
         newClass = "groundButton";
         break;
 
-      case "Ice":
+      case "ice":
         newClass = "iceButton";
         break;
 
-      case "Normal":
+      case "normal":
         newClass = "normalButton";
         break;
       
-      case "Poison":
+      case "poison":
         newClass = "poisonButton";
         break;
 
-      case "Psychic":
+      case "psychic":
         newClass = "psychicButton";
         break;
       
-      case "Rock":
+      case "rock":
         newClass = "rockButton";
         break;
         
-      case "Steel":
+      case "steel":
         newClass = "steelButton";
         break;
   
-      case "Water":
+      case "water":
         newClass = "waterButton";
         break;
     }
@@ -133,25 +155,15 @@ let pokemonRepository = (function() {
     if(typeof(pokemon) !== "object") {
       return alert("Please write a Pokemon as an object");
     }
-
-    //sorts and turns array content into strings to check validity of keys.
-    else if (Object.keys(pokemon).sort().join(" ") !== Object.keys(pokemonList[0]).sort().join(" ")) {
-      return alert("Please enter a valid Pokemon (name, types, height, weight, baseStats)");
-    }
-
-    // check that proper keys have been used for all the base stats
-    else if (Object.keys(pokemon.baseStats).sort().join(" ") !== Object.keys(pokemonList[0].baseStats).sort().join(" "))  {
-      return alert("Please enter valid base stats (hp, attack, defense, speed, spAtk, spDef)")
-    }
-
     else {pokemonList.push(pokemon)};
-  }
-  
+    }
 
 
-  function findPokemon(pokemon) {
+  function findPokemon() {
+    let input = document.getElementById("searchPokemon").value;
     let foundPokemon = pokemonList.filter(function(element){
-      return element.name.toUpperCase() === pokemon.toUpperCase()
+     return element.name.toUpperCase() === 
+      input.toUpperCase()
     });
     return foundPokemon;
     }
@@ -159,39 +171,52 @@ let pokemonRepository = (function() {
   //function to add event listener to pokemon buttons
   //"showDetails" function is wrapped in another function to prevent being called immediately
   function addListener(element, object) {
-    element.addEventListener("click", function() {showDetails(object)});
+    element.addEventListener("click", function(event) {
+      showDetails(object);
+    });
   }
   
   //some future unknown use
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function() {
+    console.log(pokemon)});
   }
 
   function addListItem(pokemon) {
     //varuables for pokemon object
     let name = pokemon.name;
-    let type1 = pokemon.types[0];
-    let type2 = pokemon.types[1];
+    let type1 = pokemon.type1;
+    let type2 = pokemon.type2;
     let height = pokemon.height;
     let weight = pokemon.weight;
-    let hp = pokemon.baseStats.hp;
-    let attack = pokemon.baseStats.attack;
-    let defense = pokemon.baseStats.defense;
-    let speed = pokemon.baseStats.speed;
-    let spAtk = pokemon.baseStats.spAtk;
-    let spDef = pokemon.baseStats.spDef;
+    let hp = pokemon.hp;
+    let attack = pokemon.attack;
+    let defense = pokemon.defense;
+    let speed = pokemon.speed;
+    let spAtk = pokemon.spAtk;
+    let spDef = pokemon.spDef;
   
     //create the list of Pokemon
     let list = document.querySelector('ul');
     let listItem = document.createElement('li');
     let pokemonButton = document.createElement('button');
     pokemonButton.innerText = name;
+    if(pokemonList.indexOf(pokemon) < 151) {
+      pokemonButton.classList.add("button1");
+    } else if(pokemonList.indexOf(pokemon) > 150 && pokemonList.indexOf(pokemon) <= 250) {
+      pokemonButton.classList.add("button2");
+    } else if(pokemonList.indexOf(pokemon) > 250 && pokemonList.indexOf(pokemon) <= 385) {
+      pokemonButton.classList.add("button3");
+    } else if(pokemonList.indexOf(pokemon) > 386) {
+      pokemonButton.classList.add("button4");
+    } 
     pokemonButton.classList.add("button");
+    pokemonButton.classList.add("hidden");
     listItem.appendChild(pokemonButton);
     list.appendChild(listItem);
     addListener(pokemonButton, pokemon);
-    getButtonColor (pokemonButton, type1)
   }
+
 
   //return the functions to give access to them
   return {
@@ -200,9 +225,14 @@ let pokemonRepository = (function() {
     findPokemon: findPokemon,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showDetails: showDetails,
+    chooseList: chooseList
   };
 }());
 
+pokemonRepository.chooseList();
 
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {pokemonRepository.addListItem(pokemon)})
+});
